71bb1946058ddd0516655f2190bcd58f16e6cd8d
StratiX0 <jerome.hussenet@sfr.fr>
2024-02-02 16:32:26 +0100

    ajout collision

--- map.cs
+++ map.cs
@@ -1,9 +1,8 @@
-using System;
+﻿using System;
 public class Map
 {
-    public static int xPos;
-    public static int yPos;
-    public static string elementPos;
+    public static int xPos, yPos;
+    public static string elementPos, player;
     public static string[] lines;
 
     // create a method to read the map
@@ -20,12 +19,16 @@ public class Map
     // create a method to spawn the player and to move the player
     public static void SpawnPlayer()
     {
-        xPos = 0;
-        yPos = 0;
+        player = "☺";
+        xPos = 156;
+        yPos = 69;
+
         // spawn the player
         Console.SetCursorPosition(xPos, yPos);
-        Console.Write("P");
+        elementPos = lines[yPos].ElementAt(xPos).ToString();
+        Console.Write(player);
         Console.SetCursorPosition(xPos, yPos);
+
         // move the player
         ConsoleKeyInfo keyInfo;
         do
@@ -33,43 +36,88 @@ public class Map
             keyInfo = Console.ReadKey();
             switch (keyInfo.Key)
             {
+                // move up
                 case ConsoleKey.UpArrow:
                     yPos -= 1;
-                    Console.SetCursorPosition(xPos, yPos);             
-                    Console.Write("P");
                     Console.SetCursorPosition(xPos, yPos);
-                    Console.SetCursorPosition(xPos, yPos + 1);
-                    Console.Write(elementPos);
-                    elementPos = lines[yPos].ElementAt(xPos).ToString();
-                    break;
+                    if (CanMove())
+                    {
+                        yPos += 1;
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write(player);
+                        Console.SetCursorPosition(xPos, yPos + 1);
+                        Console.Write(elementPos);
+                        elementPos = lines[yPos].ElementAt(xPos).ToString();
+                        break;
+                    }
+                // move down
                 case ConsoleKey.DownArrow:
                     yPos += 1;
                     Console.SetCursorPosition(xPos, yPos);
-                    Console.Write("P");
-                    Console.SetCursorPosition(xPos, yPos);
-                    Console.SetCursorPosition(xPos, yPos - 1);
-                    Console.Write(elementPos);
-                    elementPos = lines[yPos].ElementAt(xPos).ToString();
-                    break;
+                    if (CanMove())
+                    {
+                        yPos -= 1;
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write(player);
+                        Console.SetCursorPosition(xPos, yPos - 1);
+                        Console.Write(elementPos);
+                        elementPos = lines[yPos].ElementAt(xPos).ToString();
+                        break;
+                    }
+                    
+                // move left
                 case ConsoleKey.LeftArrow:
                     xPos -= 1;
                     Console.SetCursorPosition(xPos, yPos);
-                    Console.Write("P");
-                    Console.SetCursorPosition(xPos, yPos);
-                    Console.SetCursorPosition(xPos + 1, yPos);
-                    Console.Write(elementPos);
-                    elementPos = lines[yPos].ElementAt(xPos).ToString();
-                    break;
+                    if (CanMove())
+                    {
+                        xPos += 1;
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write(player);
+                        Console.SetCursorPosition(xPos + 1, yPos);
+                        Console.Write(elementPos);
+                        elementPos = lines[yPos].ElementAt(xPos).ToString();
+                        break;
+                    }
+                    
+                // move right
                 case ConsoleKey.RightArrow:
                     xPos += 1;
                     Console.SetCursorPosition(xPos, yPos);
-                    Console.Write("P");
-                    Console.SetCursorPosition(xPos, yPos);
-                    Console.SetCursorPosition(xPos - 1, yPos);
-                    Console.Write(elementPos);
-                    elementPos = lines[yPos].ElementAt(xPos).ToString();
-                    break;
+                    if (CanMove())
+                    {
+                        xPos -= 1;
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write(player);
+                        Console.SetCursorPosition(xPos - 1, yPos);
+                        Console.Write(elementPos);
+                        elementPos = lines[yPos].ElementAt(xPos).ToString();
+                        break;
+                    }
+                    
             }
         } while (keyInfo.Key != ConsoleKey.Escape);
+    }
+
+    public static bool CanMove()
+    {
+        if (lines[yPos].ElementAt(xPos).ToString() == "~" || lines[yPos].ElementAt(xPos).ToString() == "|" || lines[yPos].ElementAt(xPos).ToString() == "/" || lines[yPos].ElementAt(xPos).ToString() == "\\" || lines[yPos].ElementAt(xPos).ToString() == "(" || lines[yPos].ElementAt(xPos).ToString() == ")" || lines[yPos].ElementAt(xPos).ToString() == "_")
+        {
+            return true;
+        }
+
+        return false;
     }
 }
