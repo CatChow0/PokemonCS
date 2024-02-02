6624e45f36a8ef603778335016eb9f43b397c5c1
Kagutsuchi84 <115561782+Mattys8423@users.noreply.github.com>
2024-02-02 17:58:54 +0100

    test

--- pokemon.cs
+++ pokemon.cs
@@ -1,6 +1,8 @@
 ﻿// Class for pokemon
 
 using System;
+using System.Diagnostics.Metrics;
+using System.Drawing;
 
 // Pokemon class
 
@@ -16,11 +18,11 @@ public class Pokemon : Entity
     public static void CreatePokemon()
     {
         // Create a new enemy
-        Pokemon pikachu = new Pokemon("pikachu", 100, 10, 5, "Electric", 5, 250, false, 100, "Bolt Strike", 8, "Electro Ball", 18);
-        Pokemon salamèche = new Pokemon("salamèche", 100, 10, 5, "Fire", 5, 250, true, 100, "Nitocharge", 8, "Flame Thrower", 18);
-        Pokemon bulblizarre = new Pokemon("bulblizar", 100, 10, 5, "Grass", 5, 250, true, 100, "Seed Balls", 8, "Eco Sphere", 18);
-        Pokemon carapuce = new Pokemon("carapuce", 100, 10, 5, "Water", 5, 250, true, 100, "Aqua Tail", 8, "Water Gun", 18);
-        pokemons = new Pokemon[4] {pikachu,salamèche,bulblizarre,carapuce};
+        Pokemon pikachu = new Pokemon("pikachu", 100, 5, "Electric", 5, 250, false, 100, "Bolt Strike", 8, "Electro Ball", 18, "Quick Attack", 5, "Thunder", 18);
+        Pokemon salamèche = new Pokemon("salamèche", 140, 5, "Fire", 5, 250, false, 140, "Flamethrower", 18, "Ember", 5, "Scratch", 5, "Fire Blast", 18);
+        Pokemon bulbizarre = new Pokemon("bulbizarre", 120, 5, "Grass", 5, 250, false, 120, "Razor Leaf", 18, "Vine Whip", 5, "Tackle", 5, "Solar Beam", 18);
+        Pokemon carapuce = new Pokemon("carapuce", 160, 5, "Water", 5, 250, false, 160, "Hydro Pump", 18, "Bubble", 5, "Tackle", 5, "Surf", 18);
+        pokemons = new Pokemon[4] {pikachu,salamèche,bulbizarre,carapuce};
 
         // Return the enemy
         return;
